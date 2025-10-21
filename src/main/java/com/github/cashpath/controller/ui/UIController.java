c6e2ddfe896d2f93cf0c5619725c472c5d649e9f
okhromov <alexandrkhromov1@gmail.com>
2025-10-21 12:38:23 +0300

    modified GameController and UIController (create-game endpoint throws 500 error due to org.springframework.dao.DataIntegrityViolationException: not-null property references a null or transient value : com.github.cashpath.model.entity.Player.game)

--- src/main/java/com/github/cashpath/controller/ui/UIController.java
+++ src/main/java/com/github/cashpath/controller/ui/UIController.java
@@ -1,8 +1,12 @@
 package com.github.cashpath.controller.ui;
 
 import com.github.cashpath.exception.GameNotFoundException;
+import com.github.cashpath.model.entity.Asset;
 import com.github.cashpath.model.entity.Game;
+import com.github.cashpath.model.entity.Player;
 import com.github.cashpath.repository.GameRepository;
+import com.github.cashpath.service.GameService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,15 +14,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Controller
+@AllArgsConstructor
 public class UIController {
 
-    private final GameRepository gameRepository;
-
-    public UIController(GameRepository gameRepository) {
-        this.gameRepository = gameRepository;
-    }
+    private final GameService gameService;
 
     @GetMapping("/")
     public String index() {
@@ -27,17 +29,28 @@ public class UIController {
 
     @PostMapping("/create-game")
     public String createGame() {
-        Game game = new Game();
-        game.setPlayers(new ArrayList<>());
-        gameRepository.save(game);
+        Player p1 = new Player();
+        p1.setName("Игрок 1");
+        Player p2 = new Player();
+        p2.setName("Игрок 2");
+        Game game = gameService.createGame(List.of(p1, p2));
         return "redirect:/game/" + game.getId();
     }
 
     @GetMapping("/game/{id}")
     public String gamePage(@PathVariable Long id, Model model) {
-        Game game = gameRepository.findById(id)
-                .orElseThrow(() -> new GameNotFoundException(id));
+        Game game = gameService.findById(id);
         model.addAttribute("game", game);
-        return "game";
+        model.addAttribute("passiveIncomes", game.getPlayers().stream()
+                .map(p -> p.getAssets().stream()
+                        .mapToDouble(Asset::getMonthlyCashFlow).sum())
+                .toList());
+        return "game/game-board";
+    }
+
+    @PostMapping("/game/{id}/next-turn")
+    public String nextTurn(@PathVariable Long id) {
+        String log = gameService.playerMove(id);
+        return "redirect:/game/" + id + "?log=" + log;
     }
 }
