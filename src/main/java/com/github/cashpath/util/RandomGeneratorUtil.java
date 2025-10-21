80e859dd7d7265d63252d23934648a2207b73ceb
okhromov <alexandrkhromov1@gmail.com>
2025-10-21 12:31:16 +0300

    added RandomGeneratorUtil and game-board.html

---
+++ src/main/java/com/github/cashpath/util/RandomGeneratorUtil.java
@@ -0,0 +1,50 @@
+package com.github.cashpath.util;
+
+import com.github.cashpath.model.entity.Liability;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@Getter
+public class RandomGeneratorUtil {
+    private static final List<String> GOODS = List.of("Телефон", "Телевизор", "Ноутбук", "Стиральная машина", "Диван");
+    private final Random random = new Random();
+    private final double salary;
+    private final List<Liability> liabilities;
+
+    public RandomGeneratorUtil() {
+        this.salary = generateRandomSalary();
+        this.liabilities = generateRandomInitialLiabilities(salary);
+    }
+
+    private double generateRandomSalary() {
+        return 15000 + random.nextInt(45001);// 15000–60000
+    }
+
+    private double generateRandomMonthlyExpenses(double salary) {
+        //return salary * (0.5 + (random.nextDouble() * 0.2 - 0.1)); // 50% ±10%
+        return salary * (random.nextDouble() * 0.2 - 0.1); // ±10%
+    }
+
+    private List<Liability> generateRandomInitialLiabilities(double salary) {
+        List<Liability> result = new ArrayList<>();
+        result.add(createLiability("Жилье", salary, 0.40));
+        result.add(createLiability("Кредитная карта", salary, generateRandomMonthlyExpenses(salary)));
+        result.add(createLiability(getRandomItem(), salary, generateRandomMonthlyExpenses(salary)));
+        return result;
+    }
+
+    private Liability createLiability(String liabilityName, double salary, double percent) {
+        Liability l1 = new Liability();
+        l1.setName(liabilityName);
+        l1.setMonthlyPayment(salary * percent);
+        return l1;
+    }
+
+    private String getRandomItem() {
+        return GOODS.get(random.nextInt(GOODS.size()));
+    }
+
+}
